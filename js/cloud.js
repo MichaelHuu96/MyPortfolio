be4ab053636c7e97a0f7771f24e91bb6821da5fd
MichaelHuu <s220423@dtu.dk>
2023-02-06 20:42:47 +0100

    fixed links for the 3rd time

--- js/cloud.js
+++ js/cloud.js
@@ -54,7 +54,7 @@ function cloudpython(){
     document.querySelector('.cloud-text').innerHTML = "Python is third language I learned and is one im self taught I have couple of project I've made in this language for example the latest group project 'carrier-based rail system' in model based engeneering" ;
     rating(4);
 
-    document.querySelector('#cloud-atag').setAttribute('href',"Portfolio_items/1-Carrier_Based/Carrier-based Rail System.html");
+    document.querySelector('#cloud-atag').setAttribute('href',"Portfolio_Items/1-Carrier_Based/Carrier-based Rail System.html");
     document.querySelector('#cloud-atag').innerText = "Click here to see the project";
 }
 function cloudcsharp(){
@@ -70,7 +70,7 @@ function cloudcsharp(){
     document.querySelector('.cloud-text').innerHTML = "A database management system that we got taught in bachelor, I've used it in my bacholor project and during my internship at Kepasa" ;
     rating(3);
 
-    document.querySelector('#cloud-atag').setAttribute('href',"Portfolio_items/2-Kepasa/Kepasa.html");
+    document.querySelector('#cloud-atag').setAttribute('href',"Portfolio_Items/2-Kepasa/Kepasa.html");
     document.querySelector('#cloud-atag').innerText = "Click here to see the project";
 }
 function postgre(){
@@ -122,7 +122,7 @@ function cloudblazor(){
     document.querySelector('.cloud-header').innerHTML = "Blazor" ;
     document.querySelector('.cloud-text').innerHTML = "I worked with Blazor during my internship at kepasa, I used it to make a visualization of data stored from database" ;
     rating(3);
-    document.querySelector('#cloud-atag').setAttribute('href',"Portfolio_items/2-Kepasa/Kepasa.html");
+    document.querySelector('#cloud-atag').setAttribute('href',"Portfolio_Items/2-Kepasa/Kepasa.html");
     document.querySelector('#cloud-atag').innerText = "Click here to see the project";
 }
 
@@ -130,7 +130,7 @@ function clouddotnet(){
     document.querySelector('.cloud-header').innerHTML = "DotNet" ;
     document.querySelector('.cloud-text').innerHTML = "An open source framework, I use to use this framewrok every thime i programmed in C#. It had been used to create an userinterface during my internship at Kepasa" ;
     rating(4);
-    document.querySelector('#cloud-atag').setAttribute('href',"Portfolio_items/2-Kepasa/Kepasa.html");
+    document.querySelector('#cloud-atag').setAttribute('href',"Portfolio_Items/2-Kepasa/Kepasa.html");
     document.querySelector('#cloud-atag').innerText = "Click here to see the project";
 }
 function cloudjquery(){
