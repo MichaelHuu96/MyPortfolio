b02bab52901e606fd682e61cd856f06d2aec4905
MichaelHuu <michaelhuu1809@gmail.com>
2023-03-12 18:02:29 +0100

    updated content and added ckr portfolio

---
+++ Portfolio_Items/8-riot/test.js
@@ -0,0 +1,8 @@
+
+import { RiotAPI, RiotAPITypes, PlatformId } from '@fightmegg/riot-api'
+
+document.getElementById("demo").addEventListener("click", myFunction);
+
+function myFunction() {
+    document.getElementById("demo").innerHTML = "YOU CLICKED ME!";
+}
